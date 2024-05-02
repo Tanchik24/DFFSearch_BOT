45b1f3ea02f3870bf067c368399fd82d5b89401a
Tatiana Maslennikova <tanyamaslennikova24@mail.ru>
2024-05-02 10:42:41 +0300

    added name_extractor and slot_intent_extractor

---
+++ NLU/NameExtractor.py
@@ -0,0 +1,33 @@
+from pymystem3 import Mystem
+
+
+class NameExtractor:
+    def __init__(self):
+        self.m = Mystem()
+
+    def predict_names(self, text):
+        analyze = self.m.analyze(text)
+        first_name = None
+        second_name = None
+        middle_name = None
+
+        for word in analyze:
+            try:
+                try:
+                    analysis = word['analysis'][0]
+                except KeyError:
+                    continue
+            except IndexError:
+                continue
+
+            if 'имя' in analysis['gr']:
+                first_name = word['text'].lower()
+            elif 'фам' in analysis['gr']:
+                second_name = word['text'].lower()
+            elif 'отч' in analysis['gr']:
+                middle_name = word['text'].lower()
+
+        return second_name, first_name, middle_name
+
+
+name_extractor = NameExtractor()
---
+++ NLU/SlotIntentExtractor.py
@@ -0,0 +1,43 @@
+import os
+import json
+from typing import Dict
+from dotenv import load_dotenv
+import logging
+import requests
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+class SlotIntentExtractor:
+    def __init__(self):
+        self.cache: Dict[str, tuple] = {}
+
+    def predict_intent(self, message: str) -> str:
+        if message not in self.cache:
+            self.__get_predict_from_jointbert(message)
+        intent, proba = self.cache[message][0]
+        logging.info(f"The model recognized the intent '{intent}' with a probability of {proba:.2%}")
+        return intent if proba > 0.8 else 'out_of_scope'
+
+    def predict_slots(self, message: str) -> dict:
+        if message not in self.cache:
+            self.__get_predict_from_jointbert(message)
+        return self.cache[message][1]
+
+    def __get_predict_from_jointbert(self, message: str):
+        url = os.getenv('JOINTBERT_URL')
+        if not url:
+            raise ValueError("Environment variable JOINTBERT_URL not set")
+        try:
+            response = requests.post(url, json={'text': message})
+            response.raise_for_status()
+            data = response.json()
+            self.cache[message] = ((data['intent'], data['proba']), data['slots'])
+        except requests.RequestException as e:
+            logging.error(f"Request failed: {e}")
+        except ValueError:
+            logging.error("JSON decode error")
+
+
+slot_intent_extractor = SlotIntentExtractor()
